1d84244c32ee5e4020e637d26157923878867d4b
wcpun <puwach123@gmail.com>
2021-06-19 20:30:59 +0800

    done problem 8 atoi

---
+++ atoi/sol.go
@@ -0,0 +1,42 @@
+package atoi
+
+import (
+	"math"
+	"strings"
+)
+
+// Atoi converts a string to a 32-bit signed integer
+func Atoi(str string) int {
+	str = strings.TrimSpace(str)
+
+	if len(str) == 0 {
+		return 0
+	}
+
+	first := str[0]
+	pre := 1
+	if first == '+' {
+		pre = 1
+		str = str[1:]
+	}
+	if first == '-' {
+		pre = -1
+		str = str[1:]
+	}
+
+	num := 0
+	for _, ch := range str {
+		if ch < '0' || ch > '9' {
+			break
+		}
+		num = num*10 + (int(ch) - '0')
+		if num > math.MaxInt32 {
+			if pre == 1 {
+				return math.MaxInt32
+			}
+			return math.MinInt32
+		}
+	}
+
+	return pre * num
+}
---
+++ atoi/sol_test.go
@@ -0,0 +1,28 @@
+package atoi
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestLongestSubStr(t *testing.T) {
+	tests := map[string]struct {
+		in  string
+		out int
+	}{
+		"case 01": {"42", 42},
+		"case 02": {"-42", -42},
+		"case 03": {"   -42", -42},
+		"case 04": {"4193 with word", 4193},
+		"case 05": {"words and 987", 0},
+		"case 06": {"-91283472332", -2147483648},
+		"case 07": {"9223372036854775808", 2147483647},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			act := Atoi(test.in)
+			misc.Equals(t, test.out, act)
+		})
+	}
+}
