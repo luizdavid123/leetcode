19e67c22a2548141a9c61d9ded9411c4b4610d99
wcpun <puwach123@gmail.com>
2021-06-19 13:59:15 +0800

    done promber 6 zig zag conversion

---
+++ zigzag/sol.go
@@ -0,0 +1,37 @@
+package zigzag
+
+import "strings"
+
+// ZigZagAndToRow rewrite the string in a zigzag patten and read it one by one by row
+func ZigZagAndToRow(str string, n int) string {
+	if n == 1 || len(str) == 1 {
+		return str
+	}
+	var builder strings.Builder
+
+	cnt := len(str)
+	cyclen := 2 * (n - 1)
+
+	// first row
+	for i := 0; i < cnt; i += cyclen {
+		builder.WriteByte(str[i])
+	}
+
+	// innner row
+	for r := 1; r < n-1; r++ {
+		for i := r; i < cnt; i += cyclen {
+			builder.WriteByte(str[i])
+			j := i + 2*(n-r-1)
+			if j < cnt {
+				builder.WriteByte(str[j])
+			}
+		}
+	}
+
+	// last row
+	for i := n - 1; i < cnt; i += cyclen {
+		builder.WriteByte(str[i])
+	}
+
+	return builder.String()
+}
---
+++ zigzag/sol_test.go
@@ -0,0 +1,33 @@
+package zigzag
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestZigZagAndToRow(t *testing.T) {
+	tests := map[string]struct {
+		instr string
+		inrow int
+		out   string
+	}{
+		"case 01": {"a", 1, "a"},
+		"case 02": {"a", 5, "a"},
+		"case 03": {"ab", 1, "ab"},
+		"case 04": {"abcd", 1, "abcd"},
+		"case 05": {"abcd", 2, "acbd"},
+		"case 06": {"abcd", 3, "abdc"},
+		"case 07": {"abcd", 4, "abcd"},
+		"case 08": {"paypalishiring", 4, "pinalsigyahrpi"},
+		"case 09": {"paypalishiring", 3, "pahnaplsiigyir"},
+		"case 10": {"abcdefg", 4, "agbfced"},
+		"case 11": {"abcdefg", 3, "aebdfcg"},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			act := ZigZagAndToRow(test.instr, test.inrow)
+			misc.Equals(t, test.out, act)
+		})
+	}
+}
