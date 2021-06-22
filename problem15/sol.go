098e3dea5514ae1513c3e26d23d54c7c58badcaa
wcpun <puwach123@gmail.com>
2021-06-22 23:18:17 +0800

    done problem 15 three sum

---
+++ problem15/sol.go
@@ -0,0 +1,57 @@
+package problem15
+
+import (
+	"sort"
+)
+
+/*
+	LeetCode Problem 15: Longest Common Prefix
+	Level: Medium
+	Description: Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+	such that i != j, i != k,and j != k, and nums[i] + nums[j] + nums[k] == 0.
+*/
+
+// ThreeSum return all return all the triplets [nums[i], nums[j], nums[k]] that sum up to 0
+func ThreeSum(nums []int) [][]int {
+	ans := [][]int{}
+
+	cnt := len(nums)
+	sorted := nums
+	sort.Ints(nums)
+
+	for i := 0; i < cnt-2; i++ {
+		l, r := i+1, cnt-1
+		for l < r {
+			sum := sorted[i] + sorted[l] + sorted[r]
+			switch {
+			case sum == 0:
+				triplet := []int{sorted[i], sorted[l], sorted[r]}
+				sort.Ints(triplet)
+				if !IsAnsIncludesTripe(ans, triplet) {
+					ans = append(ans, triplet)
+				}
+				l++
+				r--
+			case sum < 0:
+				l++
+			case sum > 0:
+				r--
+			}
+		}
+	}
+
+	return ans
+}
+
+// IsAnsIncludesTripe check if ans contains ans
+func IsAnsIncludesTripe(ans [][]int, triplet []int) bool {
+	for i := 0; i < len(ans); i++ {
+		arr := ans[i]
+		if arr[0] == triplet[0] &&
+			arr[1] == triplet[1] &&
+			arr[2] == triplet[2] {
+			return true
+		}
+	}
+	return false
+}
---
+++ problem15/sol_test.go
@@ -0,0 +1,43 @@
+package problem15
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	tests := map[string]struct {
+		in  []int
+		out [][]int
+	}{
+		"case 01": {[]int{-1, 0, 1, 2, -1, -4}, [][]int{{-1, -1, 2}, {-1, 0, 1}}},
+		"case 02": {[]int{}, [][]int{}},
+		"case 03": {[]int{0}, [][]int{}},
+		"case 04": {[]int{0, 0, 0, 0}, [][]int{{0, 0, 0}}},
+		"case 05": {[]int{-2, 0, 1, 1, 2}, [][]int{{-2, 0, 2}, {-2, 1, 1}}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual := ThreeSum(test.in)
+			misc.Equals(t, test.out, actual)
+		})
+	}
+}
+
+func TestIsAnsIncludesTripe(t *testing.T) {
+	tests := map[string]struct {
+		src [][]int
+		tar []int
+		out bool
+	}{
+		"case 04": {[][]int{{0, 0, 0}, {0, 0, 0}}, []int{0, 0, 0}, true},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual := IsAnsIncludesTripe(test.src, test.tar)
+			misc.Equals(t, test.out, actual)
+		})
+	}
+}
