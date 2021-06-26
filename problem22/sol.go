c102b0aac74509d0ec5b1bf05e7a496bd1805e08
wcpun <puwach123@gmail.com>
2021-06-26 14:08:58 +0800

    done problem 22 generate parentheses

---
+++ problem22/sol.go
@@ -0,0 +1,116 @@
+package problem22
+
+import "strings"
+
+/*
+	LeetCode Problem 22: Valid Parentheses
+	Level: Medium
+	Description: Given n pairs of parentheses,
+	write a function to generate all combinations of well-formed parentheses.
+*/
+
+// GenerateValidParenthesesV2 generate all combinations of well-formed parentheses
+func GenerateValidParenthesesV2(n int) []string {
+	if n == 0 {
+		return []string{""}
+	}
+	ans := []string{}
+	for i := 0; i < n; i++ {
+		for _, left := range GenerateValidParenthesesV2(i) {
+			for _, right := range GenerateValidParenthesesV2(n - i - 1) {
+				ans = append(ans, "("+left+")"+right)
+			}
+		}
+	}
+	return ans
+}
+
+// GenerateValidParentheses generate all combinations of well-formed parentheses
+func GenerateValidParentheses(n int) []string {
+	valids := []string{}
+	pairs := GenPairBracket(n)
+	for perm := range GenAllPermStr(pairs) {
+		ans := strings.Join(perm, "")
+		if Valid(ans) {
+			if !Exist(valids, ans) {
+				valids = append(valids, ans)
+			}
+		}
+	}
+	return valids
+}
+
+// Exist check if valid is already in valids
+func Exist(valids []string, valid string) bool {
+	for i := 0; i < len(valids); i++ {
+		if valids[i] == valid {
+			return true
+		}
+	}
+	return false
+}
+
+// Valid check if the input is valid
+func Valid(perm string) bool {
+	balance := 0
+	for i := 0; i < len(perm); i++ {
+		if perm[i] == '(' {
+			balance++
+		} else {
+			balance--
+		}
+		if balance < 0 {
+			return false
+		}
+	}
+	return balance == 0
+}
+
+// GenPairBracket generate pairs of brackets
+func GenPairBracket(n int) []string {
+	pair := make([]string, n*2)
+	for i := 0; i < n; i++ {
+		pair[2*i], pair[2*i+1] = "(", ")"
+	}
+	return pair
+}
+
+// GenAllPermStr wrap GenPermStr with channel
+func GenAllPermStr(strs []string) <-chan []string {
+	permch := make(chan []string)
+	go func(permch chan []string) {
+		defer close(permch)
+		GenPermStr(permch, strs)
+	}(permch)
+	return permch
+}
+
+// GenPermStr retrun all permutation of the element in strs
+func GenPermStr(permch chan []string, strs []string) {
+	output := make([]string, len(strs))
+	copy(output, strs)
+	permch <- output
+
+	cnt := len(strs)
+	p := make([]int, cnt+1)
+	for i := 0; i < cnt+1; i++ {
+		p[i] = i
+	}
+
+	for i := 1; i < cnt; {
+		p[i]--
+		j := 0
+		if i%2 == 1 {
+			j = p[i]
+		}
+		tmp := strs[j]
+		strs[j] = strs[i]
+		strs[i] = tmp
+		output := make([]string, len(strs))
+		copy(output, strs)
+		permch <- output
+		for i = 1; p[i] == 0; i++ {
+			p[i] = i
+		}
+	}
+}
---
+++ problem22/sol_test.go
@@ -0,0 +1,48 @@
+package problem22
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestGenerateParentheses(t *testing.T) {
+	tests := map[string]struct {
+		in  int
+		out []string
+	}{
+		"case 01": {1, []string{"()"}},
+		"case 02": {2, []string{"()()", "(())"}},
+		"case 03": {3, []string{"()()()", "(())()", "(()())", "()(())", "((()))"}},
+		"case 04": {4, GenerateValidParenthesesV2(4)},
+		"case 05": {5, GenerateValidParenthesesV2(5)},
+		// "case 06": {6, GenerateValidParenthesesV2(6)},
+		// "case 08": {8, GenerateValidParenthesesV2(8)},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual := GenerateValidParentheses(test.in)
+			misc.Equals(t, true, ValidAns(test.out, actual))
+		})
+	}
+}
+
+// ValidAns check if the ans is valid
+func ValidAns(expect, actual []string) bool {
+	for i := 0; i < len(actual); i++ {
+		if !Contain(expect, actual[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Contain check if the str is in strs
+func Contain(strs []string, str string) bool {
+	for i := 0; i < len(strs); i++ {
+		if strs[i] == str {
+			return true
+		}
+	}
+	return false
+}
