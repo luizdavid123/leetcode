e67506f05ae639ec3068a6e9dfa2dfcf8c559d9c
wcpun <puwach123@gmail.com>
2021-06-27 16:33:17 +0800

    done problem 29 divide two number

---
+++ problem29/sol.go
@@ -0,0 +1,35 @@
+package problem29
+
+import "math"
+
+/*
+	LeetCode Problem 29: Divide Two Integers
+	Level: Easy
+	Description: Given two integers dividend and divisor,
+	divide two integers without using multiplication, division, and mod operator.
+*/
+
+// Divide divide two integers without using multiplication, division, and mod operator
+func Divide(dividend int, divisor int) int {
+	if dividend <= math.MinInt32 && divisor == -1 {
+		return math.MaxInt32
+	}
+
+	pos := 1
+	if dividend < 0 {
+		dividend *= -1
+		pos *= -1
+	}
+	if divisor < 0 {
+		divisor *= -1
+		pos *= -1
+	}
+
+	quo := 0
+	for dividend >= divisor && dividend > 0 {
+		dividend -= divisor
+		quo++
+	}
+
+	return quo * pos
+}
---
+++ problem29/sol_test.go
@@ -0,0 +1,28 @@
+package problem29
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestDivide(t *testing.T) {
+	tests := map[string]struct {
+		dividend int
+		divisor  int
+		quotient int
+	}{
+		"case 01": {10, 3, 3},
+		"case 02": {7, -3, -2},
+		"case 03": {0, 1, 0},
+		"case 04": {1, 1, 1},
+		"case 05": {-8, 5, -1},
+		"case 06": {-9, -4, 2},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual := Divide(test.dividend, test.divisor)
+			misc.Equals(t, test.quotient, actual)
+		})
+	}
+}
