50c5355386f14ec1517c910cc5aab0e3af83dbdc
wcpun <puwach123@gmail.com>
2021-07-09 00:18:37 +0800

    done problem 77 gen comb (n, k)

---
+++ problem77/sol.go
@@ -0,0 +1,91 @@
+package problem77
+
+/*
+	LeetCode Problem 81: Combinations
+	Level: Medium
+	Description: Given two integers n and k,
+	return all possible combinations of k numbers out of the range [1, n].
+*/
+
+// GenComb return all combs of k numbers out of the range [1, n]
+func GenComb(n int, k int) [][]int {
+	ans := [][]int{}
+	ch := make(chan []int, 1)
+	go func(ch chan []int) {
+		defer close(ch)
+		BackTrack(ch, []int{}, n, k, 1)
+	}(ch)
+	for comb := range ch {
+		ans = append(ans, comb)
+	}
+	return ans
+}
+
+// BackTrack is a helper method for GenComb
+func BackTrack(ch chan []int, cand []int, upper int, limit int, pos int) {
+	if limit == 0 {
+		ch <- DeepCopyInts(cand)
+	} else {
+		for i := pos; i <= upper; i++ {
+			cand = append(cand, i)
+			BackTrack(ch, cand, upper, limit-1, i+1)
+			cand = cand[:len(cand)-1]
+		}
+	}
+}
+
+// GenCombV2 return all combs of k numbers out of the range [1, n]
+func GenCombV2(n int, k int) [][]int {
+	ans := [][]int{}
+	ch := make(chan []int, 1)
+	go func(ch chan []int) {
+		defer close(ch)
+		ns := GenInts(n)
+		BackTrackV2(ch, []int{}, ns, k, 0)
+	}(ch)
+	for comb := range ch {
+		ans = append(ans, comb)
+	}
+	return ans
+}
+
+// BackTrackV2 is a helper method for GenComb
+func BackTrackV2(ch chan []int, cand []int, nums []int, limit int, pos int) {
+	if limit == 0 {
+		ch <- DeepCopyInts(cand)
+	} else {
+		for i := pos; i < len(nums); i++ {
+			cand = append(cand, nums[i])
+			BackTrackV2(ch, cand, nums, limit-1, i+1)
+			cand = cand[:len(cand)-1]
+		}
+	}
+}
+
+// DeepCopyInts return a copy of int slice
+func DeepCopyInts(nums []int) []int {
+	copy := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		copy[i] = nums[i]
+	}
+	return copy
+}
+
+// GenInts return an array of int from 1 up to n
+func GenInts(n int) []int {
+	nums := make([]int, n)
+	for i := 1; i < n+1; i++ {
+		nums[i-1] = i
+	}
+	return nums
+}
+
+// IsIntInInts check if int is in ins
+func IsIntInInts(nums []int, num int) bool {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == num {
+			return true
+		}
+	}
+	return false
+}
---
+++ problem77/sol_test.go
@@ -0,0 +1,25 @@
+package problem77
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestGenComb(t *testing.T) {
+	tests := map[string]struct {
+		n   int
+		k   int
+		ans [][]int
+	}{
+		"case 01": {4, 2, [][]int{{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}}},
+		"case 02": {1, 1, [][]int{{1}}},
+		"case 03": {4, 3, [][]int{{1, 2, 3}, {1, 2, 4}, {1, 3, 4}, {2, 3, 4}}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ans := GenCombV2(test.n, test.k)
+			misc.Equals(t, test.ans, ans)
+		})
+	}
+}
