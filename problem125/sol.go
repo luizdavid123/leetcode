e5b31167f52a59c0ddedad517cd535f0f403e502
wcpun <puwach123@gmail.com>
2021-07-09 21:38:15 +0800

    done problem 125 valid palindrome

---
+++ problem125/sol.go
@@ -0,0 +1,49 @@
+package problem125
+
+import (
+	"strings"
+)
+
+/*
+	LeetCode Problem 125: Valid Palindrome
+	Level: Easy
+	Description: Given a string s, determine if it is a palindrome,
+	considering only alphanumeric characters and ignoring cases.
+*/
+
+// IsPalindrome check if it is a palindrome
+func IsPalindrome(str string) bool {
+	cand := Normalize(str)
+	cnt := len(cand)
+	for i := 0; i < cnt/2; i++ {
+		if cand[i] != cand[cnt-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+// Normalize return a string containing only letters of lower case or digits
+func Normalize(str string) string {
+	var builder strings.Builder
+	for i := 0; i < len(str); i++ {
+		if IsDigitOrLetter(str[i]) {
+			builder.WriteString(strings.ToLower(str[i : i+1]))
+		}
+	}
+	return builder.String()
+}
+
+// IsDigitOrLetter check if it is a letter or a digit
+func IsDigitOrLetter(ch byte) bool {
+	switch {
+	case ch >= 'a' && ch <= 'z':
+		return true
+	case ch >= 'A' && ch <= 'Z':
+		return true
+	case ch >= '0' && ch <= '9':
+		return true
+	default:
+		return false
+	}
+}
---
+++ problem125/sol_test.go
@@ -0,0 +1,64 @@
+package problem125
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	tests := map[string]struct {
+		str string
+		is  bool
+	}{
+		"case 01": {"A man, a plan, a canal: Panama", true},
+		"case 02": {"race a car", false},
+		"case 03": {"a", true},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			is := IsPalindrome(test.str)
+			misc.Equals(t, test.is, is)
+		})
+	}
+}
+
+func TestIsDigitOrLetter(t *testing.T) {
+	tests := map[string]struct {
+		ch byte
+		is bool
+	}{
+		"case 01": {'a', true},
+		"case 02": {'A', true},
+		"case 03": {'0', true},
+		"case 04": {'l', true},
+		"case 05": {'L', true},
+		"case 06": {'8', true},
+		"case 07": {':', false},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			is := IsDigitOrLetter(test.ch)
+			misc.Equals(t, test.is, is)
+		})
+	}
+}
+
+func TestNormalize(t *testing.T) {
+	tests := map[string]struct {
+		str string
+		out string
+	}{
+		"case 01": {"A man, a plan, a canal: Panama", "amanaplanacanalpanama"},
+		"case 02": {"race a car", "raceacar"},
+		"case 03": {"", ""},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			out := Normalize(test.str)
+			misc.Equals(t, test.out, out)
+		})
+	}
+}
