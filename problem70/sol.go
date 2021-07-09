842ff36e6bf14a6b25574b885a40ded79fd6ec20
wcpun <puwach123@gmail.com>
2021-07-09 22:38:46 +0800

    done problem 70 climb stairs

---
+++ problem70/sol.go
@@ -0,0 +1,35 @@
+package problem70
+
+/*
+	LeetCode Problem 125: Climbing Stairs
+	Level: Easy
+	Description: You are climbing a staircase. It takes n steps to reach the top.
+	Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+
+// ClimbStairsV1 return the num of way climt to the top by recursion
+func ClimbStairsV1(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	return ClimbStairsV1(n-1) + ClimbStairsV1(n-2)
+}
+
+// ClimbStairsV2 return the num of way climt to the top memorization
+func ClimbStairsV2(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	for i := 0; i < n+1; i++ {
+		dp[i] = -1
+	}
+	dp[0], dp[1], dp[2] = 0, 1, 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
---
+++ problem70/sol_test.go
@@ -0,0 +1,24 @@
+package problem70
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestClimbStairs(t *testing.T) {
+	tests := map[string]struct {
+		n int
+		a int
+	}{
+		"case 01": {2, 2},
+		"case 02": {3, 3},
+		"case 03": {1, 1},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			a := ClimbStairsV2(test.n)
+			misc.Equals(t, test.a, a)
+		})
+	}
+}
