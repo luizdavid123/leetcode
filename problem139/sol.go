fc8e21f3f9c39b2a10892adf38722f7b0b08bf12
wcpun <puwach123@gmail.com>
2021-07-11 18:07:19 +0800

    done problem 139 word break

---
+++ problem139/sol.go
@@ -0,0 +1,39 @@
+package problem139
+
+/*
+	LeetCode Problem 139: Word Break
+	Level: Medium
+	Description: Given a string s and a dictionary of strings wordDict,
+	return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+*/
+
+// WordBreak return true if s can be segmented into a space-separated sequence of one or more dictionary words,
+// given a string str and a dictionary of strings dict
+func WordBreak(str string, dict []string) bool {
+	cnt := len(str)
+	dp := make([]bool, len(str)+1)
+	dp[0] = true
+
+	for i := 1; i <= cnt; i++ {
+		for j := 0; j < i; j++ {
+			cand := str[j:i]
+			if dp[j] && IsStrInStrs(dict, cand) {
+				dp[i] = true
+				break
+			}
+		}
+	}
+
+	return dp[cnt]
+}
+
+// IsStrInStrs check if an element is in an array
+func IsStrInStrs(eles []string, ele string) bool {
+	cnt := len(eles)
+	for i := 0; i < cnt; i++ {
+		if eles[i] == ele {
+			return true
+		}
+	}
+	return false
+}
---
+++ problem139/sol_test.go
@@ -0,0 +1,25 @@
+package problem139
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestWordBreak(t *testing.T) {
+	tests := map[string]struct {
+		str  string
+		dict []string
+		ok   bool
+	}{
+		"case 01": {"leetcode", []string{"leet", "code"}, true},
+		"case 02": {"applepenapple", []string{"apple", "pen"}, true},
+		"case 03": {"catsandog", []string{"cats", "dog", "sand", "and", "cat"}, false},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ok := WordBreak(test.str, test.dict)
+			misc.Equals(t, test.ok, ok)
+		})
+	}
+}
