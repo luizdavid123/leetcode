ab12bd5e262f5651fa6b2db1dfbc51deee0cf5cf
wcpun <puwach123@gmail.com>
2021-07-11 14:52:04 +0800

    done problem 119 get idx th pascal tri

---
+++ problem119/sol.go
@@ -0,0 +1,29 @@
+package problem119
+
+/*
+	LeetCode Problem 119: Pascal's Triangle II
+	Level: 119
+	Description: Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+*/
+
+// GetRowOfPascalTri return the rowIndexth (0-indexed) row of the Pascal's triangle.
+func GetRowOfPascalTri(row int) []int {
+	all := GenPascalTri(row + 1)
+	ans := all[row]
+	return ans
+}
+
+// GenPascalTri return the first num rows of Pascal's triangle.
+func GenPascalTri(nRow int) [][]int {
+	ans := [][]int{{1}}
+	for i := 1; i < nRow; i++ {
+		prev := ans[i-1]
+		row := []int{1}
+		for j := 1; j < len(prev); j++ {
+			row = append(row, prev[j-1]+prev[j])
+		}
+		row = append(row, 1)
+		ans = append(ans, row)
+	}
+	return ans
+}
---
+++ problem119/sol_test.go
@@ -0,0 +1,25 @@
+package problem119
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestGetRowOfPascalTri(t *testing.T) {
+	tests := map[string]struct {
+		row int
+		ans []int
+	}{
+		"case 01": {0, []int{1}},
+		"case 02": {1, []int{1, 1}},
+		"case 03": {2, []int{1, 2, 1}},
+		"case 04": {3, []int{1, 3, 3, 1}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ans := GetRowOfPascalTri(test.row)
+			misc.Equals(t, test.ans, ans)
+		})
+	}
+}
