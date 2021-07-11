94b7b4235e87ef1afae964cc2307607f30b7b6b1
wcpun <puwach123@gmail.com>
2021-07-12 00:15:33 +0800

    done problem 131 palindrome partitioning

---
+++ problem131/sol.go
@@ -0,0 +1,60 @@
+package problem131
+
+/*
+	LeetCode Problem 131: Palindrome Partitioning
+	Level: Medium
+	Description: Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+	Link: https://leetcode.com/problems/palindrome-partitioning/
+*/
+
+// Partition return all possible partitioning of str such that
+// every substring of the  partition is a palindrome.
+func Partition(str string) [][]string {
+	ans := [][]string{}
+	ch := make(chan []string)
+	go func(ch chan []string) {
+		defer close(ch)
+		BackTrack(ch, str, []string{}, 0)
+	}(ch)
+	for cand := range ch {
+		ans = append(ans, cand)
+	}
+	return ans
+}
+
+// BackTrack is the helper method for Partition
+func BackTrack(ch chan []string, str string, cand []string, start int) {
+	if start >= len(str) {
+		ch <- DeepCopyStrs(cand)
+		return
+	}
+	for i := start; i < len(str); i++ {
+		tmp := str[start : i+1]
+		if IsPalindrome(tmp) {
+			cand = append(cand, tmp)
+			BackTrack(ch, str, cand, i+1)
+			cand = cand[:len(cand)-1]
+		}
+	}
+}
+
+// DeepCopyStrs return a copy of string slice
+func DeepCopyStrs(strs []string) []string {
+	copy := make([]string, len(strs))
+	for i := 0; i < len(strs); i++ {
+		copy[i] = strs[i]
+	}
+	return copy
+}
+
+// IsPalindrome check if it is a palindrome
+func IsPalindrome(str string) bool {
+	cand := str
+	cnt := len(cand)
+	for i := 0; i < cnt/2; i++ {
+		if cand[i] != cand[cnt-i-1] {
+			return false
+		}
+	}
+	return true
+}
---
+++ problem131/sol_test.go
@@ -0,0 +1,24 @@
+package problem131
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestPartition(t *testing.T) {
+	tests := map[string]struct {
+		str string
+		ans [][]string
+	}{
+		"case 01": {"aab", [][]string{{"a", "a", "b"}, {"aa", "b"}}},
+		"case 02": {"a", [][]string{{"a"}}},
+		"case 03": {"", [][]string{{}}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ans := Partition(test.str)
+			misc.Equals(t, test.ans, ans)
+		})
+	}
+}
