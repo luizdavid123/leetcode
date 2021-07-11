79bd9d4d35097b7ccb6d4b164ff7b15f12f32e87
wcpun <puwach123@gmail.com>
2021-07-11 21:49:59 +0800

    done problem 128 longest consective ele seq

---
+++ problem128/sol.go
@@ -0,0 +1,46 @@
+package problem128
+
+/*
+	LeetCode Problem 128: Longest Consecutive Sequence
+	Level: Medium
+	Description: Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+	Link: https://leetcode.com/problems/longest-consecutive-sequence/
+*/
+
+// LongestConsective return the length of the longest consecutive elements sequence.
+func LongestConsective(nums []int) int {
+	set := map[int]int{}
+	for _, num := range nums {
+		cnt, ok := set[num]
+		if !ok {
+			set[num] = 0
+		}
+		set[num] = cnt + 1
+	}
+	ans := 0
+	for num := range set {
+		if _, ok := set[num-1]; !ok {
+			curr := num
+			streak := 1
+			for {
+				if _, ok := set[curr+1]; ok {
+					curr += 1
+					streak += 1
+					continue
+				} else {
+					break
+				}
+			}
+			ans = MaxInt(ans, streak)
+		}
+	}
+	return ans
+}
+
+// MaxInt return the larger one
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ problem128/sol_test.go
@@ -0,0 +1,25 @@
+package problem128
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestLongestConsective(t *testing.T) {
+	tests := map[string]struct {
+		nums []int
+		ans  int
+	}{
+		"case 01": {[]int{100, 4, 200, 1, 3, 2}, 4},
+		"case 02": {[]int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}, 9},
+		"case 03": {[]int{0}, 1},
+		"case 04": {[]int{}, 0},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ans := LongestConsective(test.nums)
+			misc.Equals(t, test.ans, ans)
+		})
+	}
+}
