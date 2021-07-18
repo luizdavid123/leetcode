fbbc31b5e944dba340fe056b09f24838c5d23ff9
wcpun <puwach123@gmail.com>
2021-07-18 21:04:39 +0800

    done problem 168 excel sheet column title

--- problem168/sol.go
+++ problem168/sol.go
@@ -1,1 +1,39 @@
 package problem168
+
+import "strings"
+
+/*
+	LeetCode Problem 168: Excel Sheet Column Title
+	Level: Easy
+	Description: Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+	Link: https://leetcode.com/problems/excel-sheet-column-title/
+*/
+
+// ConvertToTitle return its corresponding column title as it appears in an Excel sheet given
+// an integer colnum
+func ConvertToTitle(col int) string {
+	ans := ""
+	d := col
+	for d > 0 {
+		d--
+		ans += GetTitleChar(d % 26)
+		d /= 26
+	}
+	return RvsStr(ans)
+}
+
+// GetTitleChar return single char between A-Z based on the given num in range 1-26
+func GetTitleChar(num int) string {
+	chars := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	return chars[num]
+}
+
+// RvsStr reverse string
+func RvsStr(str string) string {
+	var rvs strings.Builder
+	cnt := len(str)
+	for i := cnt - 1; i >= 0; i-- {
+		rvs.WriteByte(str[i])
+	}
+	return rvs.String()
+}
--- problem168/sol_test.go
+++ problem168/sol_test.go
@@ -1,1 +1,42 @@
 package problem168
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestConvertToTitle(t *testing.T) {
+	tests := map[string]struct {
+		colnum int
+		title  string
+	}{
+		"case 01": {1, "A"},
+		"case 02": {28, "AB"},
+		"case 03": {701, "ZY"},
+		"case 04": {2147483647, "FXSHRXW"},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			title := ConvertToTitle(test.colnum)
+			misc.Equals(t, test.title, title)
+		})
+	}
+}
+
+func TestGetTitleChar(t *testing.T) {
+	tests := map[string]struct {
+		num  int
+		char string
+	}{
+		"case 01": {1, "A"},
+		"case 02": {26, "Z"},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			char := GetTitleChar(test.num)
+			misc.Equals(t, test.char, char)
+		})
+	}
+}
