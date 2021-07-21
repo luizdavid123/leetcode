315578b08525f642f766ae6fdf0dd3ee892c664f
wcpun <puwach123@gmail.com>
2021-07-21 22:16:09 +0800

    done problem 162 find peak ele

---
+++ problem162/sol.go
@@ -0,0 +1,24 @@
+package problem162
+
+/*
+	LeetCode Problem 162: Find Peak Element
+	Level: Medium
+	Description: A peak element is an element that is strictly greater than its neighbors.
+	Given an integer array nums, find a peak element, and return its index.
+	If the array contains multiple peaks, return the index to any of the peaks
+	Link: https://leetcode.com/problems/find-peak-element/
+*/
+
+// FindPeakEle find a peak element and return its index
+func FindPeakEle(nums []int) int {
+	l, r := 0, len(nums)-1
+	for l < r {
+		m := l + (r-l)/2
+		if nums[m] > nums[m+1] {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
---
+++ problem162/sol_test.go
@@ -0,0 +1,24 @@
+package problem162
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestFindPeakEle(t *testing.T) {
+	tests := map[string]struct {
+		nums []int
+		peak int
+	}{
+		"case 01": {[]int{1, 2, 3, 1}, 2},
+		"case 02": {[]int{1, 2, 1, 3, 5, 6, 4}, 5},
+		"case 03": {[]int{2}, 0},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			peak := FindPeakEle(test.nums)
+			misc.Equals(t, test.peak, peak)
+		})
+	}
+}
