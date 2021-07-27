19c7dac3a89d4fde16d5ae53ea2426bc140b56fe
wcpun <puwach123@gmail.com>
2021-07-27 23:33:57 +0800

    done problem 215 k th max

---
+++ problem215/sol.go
@@ -0,0 +1,48 @@
+package problem215
+
+import "sort"
+
+/*
+	LeetCode Problem 215: Kth Largest Element in an Array
+	Level: Medium
+	Description: Given an integer array nums and an integer k, return the kth largest element in the array.
+	Link: https://leetcode.com/problems/kth-largest-element-in-an-array/
+*/
+
+// FindKthTop return the kth largest element in the array.
+func FindKthTop(nums []int, k int) int {
+	sort.Ints(nums)
+	return nums[len(nums)-k]
+}
+
+// FindKthTop return the kth largest element in the array.
+func FindKthTopV2(nums []int, k int) int {
+	k = len(nums) - k
+	high, low := 0, len(nums)-1
+	for low < high {
+		pivot := Partition(nums, high, low)
+		if pivot < k {
+			low = pivot + 1
+		} else if pivot > k {
+			high = pivot - 1
+		} else {
+			break
+		}
+	}
+	return nums[k]
+}
+
+// Partition divide the arr into left half and right half, nums in the left half <= pivot,
+// nums in the right half > pivot
+func Partition(nums []int, high int, low int) int {
+	pivot := nums[high]
+	i := low
+	for j := low; j < high; j++ {
+		if nums[j] <= pivot {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+	}
+	nums[high], nums[i] = nums[i], nums[high]
+	return i
+}
---
+++ problem215/sol_test.go
@@ -0,0 +1,25 @@
+package problem215
+
+import (
+	"leetcode/misc"
+	"testing"
+)
+
+func TestFindKthTop(t *testing.T) {
+	tests := map[string]struct {
+		nums []int
+		k    int
+		kmax int
+	}{
+		"case 01": {[]int{3, 2, 1, 5, 6, 4}, 2, 5},
+		"case 02": {[]int{3, 2, 3, 1, 2, 5, 4, 5, 6}, 4, 4},
+		"case 03": {[]int{1}, 1, 1},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			kmax := FindKthTopV2(test.nums, test.k)
+			misc.Equals(t, test.kmax, kmax)
+		})
+	}
+}
